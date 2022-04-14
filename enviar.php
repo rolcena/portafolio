48bd5c28eea6c423222ec4f377d71f94d2cc5264
rolcena <rolcena@hotmail.com>
2022-04-14 14:02:50 -0400

    s

--- enviar.php
+++ enviar.php
@@ -4,8 +4,8 @@
     $nombre     =   $_POST["name"];
     $correo     =   $_POST["email"];
     $mensaje    =   $_POST["message"];
-    $contenido  =   "Nombre: " . $nombre . "\nCorreo: " . $correo . "\Mensaje: " . $mensaje;
+    $contenido  =   "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
 
     mail($destino,"Contacto via ROLCENA.GA", $contenido);  
-
+    header("Location:index.html")
 ?>
