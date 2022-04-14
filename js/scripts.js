dbdfab1f9a47753b24c99233c24ece982cf90002
rolcena <rolcena@hotmail.com>
2022-04-14 16:14:47 -0400

    das

--- js/scripts.js
+++ js/scripts.js
@@ -110,7 +110,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I am A Freelancer.", "I Design Websites.", "I Live in New York."],    /* You can change the home section typing text from
+            strings: ["I am A Freelancer.", "I am A Cerrista." , "I am A engineering student" , "I Design Websites.", "I Live in Paraguay."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 50
           });
